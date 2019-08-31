344c59a27a6baf911172a9936c1e81d8cc355a35
yuliya <yuliya.sm7@gmail.com>
2019-08-31 20:30:47 +0300

    optimized

--- source/format.js
+++ source/format.js
@@ -2,25 +2,16 @@ function format(array, columns) {
     let rows = Math.ceil(array.length/columns);
     let matrix  = [rows];
     let column_width = new Array(columns).fill(0);
-    for(let i = 0; i<rows; i++) {
+    for(let i = 0; i<rows; i++) {  // заполнение матрицы
         matrix[i] = [columns];
         for(let j = 0; j < columns && (i*columns+j)< array.length; j++){
             matrix[i][j] = array[i*columns+j].toString();
-            if(matrix[i][j].length > column_width[j]){
-                column_width[j] = matrix[i][j].length;
-            }
+            column_width[j] = Math.max(matrix[i][j].length, column_width[j]);
         }
     }
-    let output = "";
-    for(let i = 0; i<rows; i++) {
-        for(let j = 0; j < columns && j< matrix[i].length; j++){
-            let space_count = column_width[j] - matrix[i][j].length + Math.sign(j);
-            output += " ".repeat(space_count);
-            output += matrix[i][j];
-        }
-        if (i < rows-1){
-            output += '\n';
-        }
+    for(let i = 0; i<rows; i++) {  // форматирование матрицы для вывода
+        matrix[i] = matrix[i].map((val, j) => ' '.repeat(column_width[j] - val.length) + val);
+        matrix[i] = matrix[i].join(' ');
     }
-    return output;
+    return matrix.join('\n');
 }
